aae27ce9e611bb65c6dedc8085f1c9207dcab8c2
Lucas <lucasmoc47@gmail.com>
2020-02-18 14:54:40 -0300

    Accessing Theme Context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 
+import { ThemeContext } from '../contexts/ThemeContext'
+
 export default class Navbar extends Component {
-  render() {
-	return (
-		<nav>
-			<h1>Context App</h1>
-			<ul>
-				<li>Home</li>
-				<li>About</li>
-				<li>Contact</li>
-			</ul>
-		</nav>
-	)
-  }
+	static contextType = ThemeContext
+
+	render() {
+		const { isLightTheme, light, dark } = this.context
+		const theme = isLightTheme ? light : dark
+
+		return (
+			<nav style={{ background: theme.ui, color: theme.synthax }}>
+				<h1>Context App</h1>
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+		)
+	}
 }
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,6 +1,6 @@
 import React, { Component, createContext } from 'react';
 
-const ThemeContext = createContext()
+export const ThemeContext = createContext()
 
 export default class ThemeContextProvider extends Component {
 	constructor(props) {
