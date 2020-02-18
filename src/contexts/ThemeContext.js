9eabe45dec41d889654ab9474e19baa568ec5609
Lucas <lucasmoc47@gmail.com>
2020-02-18 15:12:52 -0300

    Adding the Theme Toggle functionality

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -13,9 +13,13 @@ export default class ThemeContextProvider extends Component {
 		}
 	}
 
+	toggleTheme = () => {
+		this.setState({ isLightTheme: !this.state.isLightTheme })
+	}
+
 	render() {
 		return (
-			<ThemeContext.Provider value={{...this.state}}>
+			<ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
 				{this.props.children}
 			</ThemeContext.Provider>
 		)
