7b701a2ebdc2bd6769c0437de666555e6b56ed64
Lucas <lucasmoc47@gmail.com>
2020-02-18 15:20:59 -0300

    Setting up the Auth Context

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 
 import Navbar from './components/Navbar'
 import BookList from './components/BookList'
+import ThemeToggle from './components/ThemeToggle';
 
 import ThemeContextProvider from './contexts/ThemeContext'
-import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext'
 
 function App() {
 	return (
 		<div className="App">
 			<ThemeContextProvider>
-				<Navbar />
-				<BookList />
-				<ThemeToggle />
+				<AuthContextProvider>
+					<Navbar />
+					<BookList />
+					<ThemeToggle />
+				</AuthContextProvider>
 			</ThemeContextProvider>
 		</div>
 	);
