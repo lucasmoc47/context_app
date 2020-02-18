d7786b28ab055b215b7f12c29cabbd611ba30351
Lucas <lucasmoc47@gmail.com>
2020-02-18 14:41:52 -0300

    Adding the Context Provider

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,23 @@
+import React, { Component, createContext } from 'react';
+
+const ThemeContext = createContext()
+
+export default class ThemeContextProvider extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			isLightTheme: true,
+			light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
+			dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
+		}
+	}
+
+	render() {
+		return (
+			<ThemeContext.Provider value={{...this.state}}>
+				{this.props.children}
+			</ThemeContext.Provider>
+		)
+	}
+}
